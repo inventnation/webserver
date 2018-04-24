beef3d2a00ce16d1a45daddd19e6929facee8615
inventnation <inventnation@users.noreply.github.com>
2018-04-23 20:50:19 -0400

    Update main server.py

--- main server.py
+++ main server.py
@@ -1,98 +1,87 @@
-from socket import *
+import socket,time
+from threading import Thread
+from subprocess import call
 
-s=socket(AF_INET,SOCK_STREAM)
-host="127.0.0.1"#change this to your real ip address so i can work
-port=10000
-s.bind((host,port))#binds host and port so i can be used to check for connections on that port and communicate with that ip address
-s.listen(5)#waits for one person to connect to start and waits for another while handleing 1 person
-
-server_ip=[]#holds server ip so the the admins can see what server needs to be disconnected
-admin_name=[]#holds the name they give to the main server so they can talk
-servers=[]#hold servers for scaning and testing responsiveness
-admin_clients=[]#holds admins so they can chat to each other to see what they need to get done
-connecting=[]
-
-def send(c,msg):
-  c.send(bytes(str(msg),'utf-8'))
-
-def recive(c):
-  data=c.recv(1024)
-  data=data.decode('utf-8')
-  return data
-
-def handler():
-  while True:
-    command=recive(c)
-    if "Server: "in command[0:8]:
-      command(c,command[8:])
-    elif "Admin: "in command[0:7]:
-      admin_commands(c,command[7:])
-
-def command(c,command):
-  if command=="test":
-    send(c,"responsive!")
-  elif "scan "in command[0:5]:
-    scanning=command[5:]
-    if "chatroom "in scaning[0:9]:
-      find=0
-      for server in servers:
-        send(server,"got: "+str(scaning[9:]))
-        ans=recive(server)
-        if ans=="yes":
-          print("Found a chatroom!")
-          find+=1
-          break
-      if find==0:
-        print("Chatroom not found!")
-		elif "webserver "in scaning[0:10]:
-			find=0
-			for server in servers:
-				send(server,"got: "+str(scaning[10:]))
-				ans=recive(server)
-				if ans=="yes":
-					print("Found a webserver")
-					find+=1
-					break
-			if find==0:
-				print("Webserver not found")
-
-def admin_commands(c,command):
-  if command=="test":
-		print("responsive!")
-	elif "list "in command[0:5]:
-		if command[5:]=="admins":
-			for admin in admin_name:
-				print(str(admin)+"\n")
-		elif command[5:]=="servers":
-			for server in server_ip:
-				print(str(server)+"\n")
-
+servers={}
+connecting_server=[]
+admins_connection={}
+all_servers=[]
 def connection_checker(c,a):
-  send(c,"type?")
-  ans=recive(c)
-  if ans=="server":
-    servers.append(c)
-		server_ip.append(a[0])
-    print("Server: "+a[0]+" connected on port: "+a[1])
-  elif ans=="admin":
-    admin_clients.append(c)
-		done=0
-		name=""
-		while done!=1:
-			send(c,"name?")
-			ans=recive(c)
-			checker=str(ans).replace(" ","")
-			if checker=="":
-				send(c,"ERROR,NAME,NO BLANKS!")
-			else:
-				name+=str(ans)
-				send(c,"Welcome: "+str(name))
-				done+=1
-    print("Admin: "+str(name)+" is connected!")
-
+  for connection in connecting_server:
+    try:
+      connection.send(bytes("type?",'utf-8'))
+      data3=connection.recv(1024)
+      data3=data3.decode('utf-8')
+      if data3=="Server":
+        servers[int(a[1])]=c
+        print("Server: "+str(a[0])+" : "+str(a[1])+" is connected!")
+      elif data3=="Admin client":
+        admins_connection[int(a[1])]=c
+        print("Admin Client: "+str(a[0])+":"+str(a[1])+" is connected!")
+    except:
+      print("Connection: "+str(a[0])+" : "+str(a[1])+" disconnected")
+def commands(c,a,command):
+  if "scan for "in command[0:9]:
+    holder=command[9:]
+    if "chatroom "in holder[0:9]:
+      holder2=holder[9:]
+      for server in all_servers:
+        server.send(bytes("got chatroom: "+str(holder2),'utf-8'))
+        ans=server.recv(1024)
+        ans=server.decode('utf-8')
+        if ans=="got it":
+          print("serch was found")
+        elif ans=="no":
+          pass
+  elif command=="test":
+    print("responsive!")
+  elif "list "in command[0:5]:
+    holder2=command[5:]
+    if holder2=="users":
+      for server in all_servers:
+        server.send(bytes("list users",'utf-8'))
+        ans=server.recv(1024)
+        ans=server.decode('utf-8')
+        print(str(ans).replace("[","").replace("]","").replace(",","\n"))
+      print("done")
+def admin_commands(c,a,command):
+  holder=command
+  if "shutdown server "in holder[0:16]:
+    
+    shutdown_holder=holder[16:]
+    try:
+      sock_close=all_servers[int(shutdown_holder)]
+      all_servers.remove(sock_close)
+      c.send(bytes("Main Server:done",'utf-8'))
+    except:
+      c.send(bytes("Main Server:ERROR: it does not exist in that number!",'utf-8'))
+  elif holder=="test":
+    print("responsive!")
+def handler(c,a):
+  while True:
+    try:
+      data=c.recv(2048)
+      data=data.decode('utf-8')
+    except:
+      print("Connection: "+str(a[0])+" : "+str(a[1])+" disconnected")
+      break
+    if "Admin "in data[0:6]:
+      holder=data[6:]
+      admin_commands(c,a,holder)
+    elif "Server: "in data[0:8]:
+      holder=data[8:]
+      commands(c,a,holder)
+host="0.0.0.0"
+port=10500
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(5)
 while True:
   c,a=s.accept()
-  connecting.append(c)
-  conection_checker(c,a)
-  connectiong.remove(c)
-  handler()
+  connecting_server.append(c)
+  connection_checker(c,a)
+  all_servers.append(c)
+  connecting_server.remove(c)
+  a1=Thread(target=handler,args=(c,a))
+  a1.demon=True
+  a1.start()
