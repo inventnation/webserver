24feb9ceef51185d1e3df414cd21670ea7bca5b4
inventnation <inventnation@users.noreply.github.com>
2018-04-27 13:56:14 -0400

    Update client communicator.py

--- client communicator.py
+++ client communicator.py
@@ -1,15 +1,25 @@
 from socket import *
+import socket
 
 s=socket(AF_INET,SOCK_STREAM)
 host="0.0.0.0"
 port=10000
 s.bind((host,port))
+s2=socket(AF_INET,SOCK_STREAM)
+address=socket.gethostbyname(socket.)
+s2.connect((address,10500))
 
 def send(c,msg):
-  c.send(bytes(str(msg),'uft-8')
+  c.send(bytes(str(msg),'uft-8'))
 
 def recive(c,a):
   data=c.recv(1024)
   data=data.decode('utf-8')
   return data
 
+def handler(c,a):
+	pass
+
+while True:
+	c,a=s.acccept()
+	handler(c,a)
